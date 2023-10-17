f000a2bab16f2bf6de77daad8f5b8393cead784b
PrasannaKumar RG <78910432+prasannarg-barb@users.noreply.github.com>
2023-10-17 14:17:04 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
 
+streamlit.title("Welcome to our Data App")
+
+streamlit.header("Header Text")
+streamlit.text("Plain Text1")
+streamlit.text("Plain Text2")
+streamlit.text("Plain Text3")
