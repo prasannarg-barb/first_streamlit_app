6b69f4baf48033597c3a4d758b5e7d6e5b16d3b4
PrasannaKumar RG <78910432+prasannarg-barb@users.noreply.github.com>
2023-10-17 14:20:56 +1000

    Update streamlit_app.py for user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.header("Header Text")
 streamlit.text("Plain Text1")
 streamlit.text("Plain Text2")
 streamlit.text("Plain Text3")
+
+
+name = input("Enter your name: ")
+print("Hello " + name + " !")
