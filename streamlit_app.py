061ed82a048d9df7005bd617bd74299e62507cda
PrasannaKumar RG <78910432+prasannarg-barb@users.noreply.github.com>
2023-10-17 14:29:46 +1000

    Update streamlit_app.py TQ and QLD price calc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit as st
 
 streamlit.title("Welcome to our Data App")
 
@@ -10,3 +10,11 @@ streamlit.text("Plain Text3")
 
 name = input("Enter your name: ")
 print("Hello " + name + " !")
+
+
+TQ_curr_price = st.number_input('Enter TQ Current Price:')
+TQ_target_price = st.number_input('Enter TQ Target Price:')
+QLD_curr_price = st.number_input('Enter QLD Current Price:')
+QLD_target_price = QLD_curr_price+ (((TQ_target_price - TQ_curr_price ) / TQ_curr_price)*(2/3)*QLD_curr_price)
+
+st.write('The QLD Target Price is::', QLD_target_price)
