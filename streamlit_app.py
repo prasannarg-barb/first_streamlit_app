4c320e6e2b87c5b676a1da3dba08c5d46bb300a3
PrasannaKumar RG <78910432+prasannarg-barb@users.noreply.github.com>
2023-10-18 08:54:56 +1000

    Update streamlit_app.py - pandas to read S3 to list fruits in a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
+import pandas
+
 streamlit.title("Snowflake - Streamlit : Healthy Food Restaurant")
 
 streamlit.header('Breakfast Menu')
@@ -10,4 +12,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
 
